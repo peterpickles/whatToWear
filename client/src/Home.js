871205f93466f8ba981a45df746455e1c233b1b0
rallysport206 <phan.tony125@gmail.com>
2018-01-24 16:46:55 -0800

    style update

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+const pushup = require('./img/pushup.png')
 
 class Home extends Component {
   render(){
     return (
         <div className="container">
           <div className="search-area">
-            
-            <div className="box box__search">
-              <input className="box--search" type="text" placeholder="What do you need to undo?" />
+
+            <div className="box box__search-bar">
+              <input className="box--search-bar" type="text" placeholder="What do you need to undo?" />
             </div>
-            
+
             <div className="box box__search-content">
               <img className="box--img__search" src="https://images-na.ssl-images-amazon.com/images/I/81DFDndTFOL._SL1500_.jpg" alt="Donut"/>
               <div className="box--content__name">
@@ -21,29 +22,29 @@ class Home extends Component {
                 <p>200</p>
               </div>
             </div>
-          
+
           </div>
           <div className="exercise-area">
             <div className="box box__exercise">
-              <img className="box--img__exercise" src="https://www.fillmurray.com/200/300" alt="Pushups"/>
+              <img className="box--img__exercise" src = {String(pushup)} alt="Pushups"/>
               <div className="box--content__exercise-wrapper">
                 <h4 className="box--content__exercise">Pushups</h4>
                 <h2 className="box--content__time">34min</h2>
               </div>
             </div>
-            
+
             <div className="box box__exercise">
-              <img className="box--img__exercise" src="https://www.fillmurray.com/200/300" alt="Squats"/>
+              <img className="box--img__exercise" src="/img/squat.png" alt="Squats"/>
               <div className="box--content__exercise-wrapper">
                 <h4 className="box--content__exercise">Squats</h4>
                 <h2 className="box--content__time">24min</h2>
               </div>
             </div>
-            
+
             <div className="box box__exercise">
-              <img className="box--img__exercise" src="https://www.fillmurray.com/200/300" alt="Sex"/>
+              <img className="box--img__exercise" src="../running.png" alt="Running"/>
               <div className="box--content__exercise-wrapper">
-                <h4 className="box--content__exercise">Sexytime</h4>
+                <h4 className="box--content__exercise">Running</h4>
                 <h2 className="box--content__time">13min</h2>
               </div>
             </div>
